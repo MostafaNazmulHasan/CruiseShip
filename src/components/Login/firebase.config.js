846d5016c170997f3720668b3971d7067117d07e
MostafaNazmulHasan <mostafanazmulhasan@gmail.com>
2021-10-19 11:26:54 +0600

    cruise-ship

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFYJQT9oGY6BVHsYNv_Ek3YYn8S3h8PME",
+    authDomain: "cruise-ship-fd953.firebaseapp.com",
+    projectId: "cruise-ship-fd953",
+    storageBucket: "cruise-ship-fd953.appspot.com",
+    messagingSenderId: "950924924649",
+    appId: "1:950924924649:web:4005d79629b5d6fbb60278"
+  };
+export default firebaseConfig;
